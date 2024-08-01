a78336f702aa2dca9049fc4fae1867191dd83888
mathensousaa <mhenrique.sousa43@gmail.com>
2024-08-01 08:43:28 -0300

    feat: add cors

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-import express from 'express';
-import userRoutes from './routes/userRoutes.js';
-import expenseRoutes from './routes/expenseRoutes.js';
-import categoryRoutes from './routes/categoryRoutes.js';
+import express from "express";
+import cors from "cors";
+import userRoutes from "./routes/userRoutes.js";
+import expenseRoutes from "./routes/expenseRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
 
 const app = express();
 
 app.use(express.json());
-app.use('/api', userRoutes);
-app.use('/api', expenseRoutes);
-app.use('/api', categoryRoutes);
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+app.use("/api", userRoutes);
+app.use("/api", expenseRoutes);
+app.use("/api", categoryRoutes);
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-  console.log('Link: http://localhost:3000/');
+  console.log("Server is running on port 3000");
+  console.log("Link: http://localhost:3000/");
 });
