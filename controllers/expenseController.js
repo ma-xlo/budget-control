b0fd57fc5c0d3ddaa18320b6fe7e3578b4034062
mathensousaa <mhenrique.sousa43@gmail.com>
2024-08-05 02:28:43 -0300

    feat: add quantity to expenses total by user and expenses total by month

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -1,31 +1,31 @@
-import { validateAuthorization, getMonthName } from '../utils/helpers.js'
-import { Op } from 'sequelize';
-import Expense from '../models/expense.js';
-import Category from '../models/category.js';
-import User from '../models/user.js';
+import { validateAuthorization, getMonthName } from "../utils/helpers.js";
+import { Op } from "sequelize";
+import Expense from "../models/expense.js";
+import Category from "../models/category.js";
+import User from "../models/user.js";
 
-export async function createExpense (req, res) {
-  const user = validateAuthorization(req.headers.authorization)
+export async function createExpense(req, res) {
+  const user = validateAuthorization(req.headers.authorization);
 
-  if(!user) {
-    return res.status(401).json({message: "Unauthorized"})
+  if (!user) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
   try {
-    const payload = req.body
-    payload.userId =  user.id
+    const payload = req.body;
+    payload.userId = user.id;
     const expense = await Expense.create(payload);
     return res.status(201).json(expense);
   } catch (error) {
     return res.status(400).json({ error: error.message });
   }
-};
+}
 
-export async function getAllExpenses (req, res) {
-  const user = validateAuthorization(req.headers.authorization)
-  console.log(user)
-  if(!user) {
-    return res.status(401).json({message: "Unauthorized"})
+export async function getAllExpenses(req, res) {
+  const user = validateAuthorization(req.headers.authorization);
+  console.log(user);
+  if (!user) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
   try {
@@ -34,169 +34,174 @@ export async function getAllExpenses (req, res) {
   } catch (error) {
     return res.status(400).json({ error: error.message });
   }
-};
+}
 
+export async function deleteExpense(req, res) {
+  const user = validateAuthorization(req.headers.authorization);
 
-export async function deleteExpense(req, res){
-  const user = validateAuthorization(req.headers.authorization)
-
-  if(!user) {
-    return res.status(401).json({message: "Unauthorized"})
+  if (!user) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
   try {
     const expenseId = req.params.id;
     const expense = await Expense.findByPk(expenseId);
-    if(!expense) {
-      return res.status(404).json({message: "Not found"})
+    if (!expense) {
+      return res.status(404).json({ message: "Not found" });
     }
 
     await expense.destroy();
-    return res.status(200).json({message: "Deleted Successfully"})
-
+    return res.status(200).json({ message: "Deleted Successfully" });
   } catch (error) {
-    return res.status(500).json({error: error.message})
+    return res.status(500).json({ error: error.message });
   }
 }
 
-export async function updateExpense(req, res){
-  const user = validateAuthorization(req.headers.authorization)
+export async function updateExpense(req, res) {
+  const user = validateAuthorization(req.headers.authorization);
 
-  if(!user) {
-    return res.status(401).json({message: "Unauthorized"})
+  if (!user) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
   try {
     const expenseId = req.params.id;
     const expense = await Expense.findByPk(expenseId);
-    
-    if(!expense) {
-      return res.status(404).json({message: "Not found"})
+
+    if (!expense) {
+      return res.status(404).json({ message: "Not found" });
     }
 
-    await Expense.update(req.body,{where: {id: expenseId}});
+    await Expense.update(req.body, { where: { id: expenseId } });
 
     const updatedExpense = await Expense.findByPk(expenseId);
     return res.status(200).json(updatedExpense);
-    
   } catch (error) {
-    return res.status(500).json({error: error.message})
+    return res.status(500).json({ error: error.message });
   }
 }
 
-export async function getExpense(req, res){
-  const user = validateAuthorization(req.headers.authorization)
+export async function getExpense(req, res) {
+  const user = validateAuthorization(req.headers.authorization);
 
-  if(!user) {
-    return res.status(401).json({message: "Unauthorized"})
+  if (!user) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
-  
+
   try {
     const expenseId = req.params.id;
     const expense = await Expense.findByPk(expenseId);
-    if(!expense) {
-      return res.status(404).json({message: "Not found"})
+    if (!expense) {
+      return res.status(404).json({ message: "Not found" });
     }
 
-    return res.status(200).json(expense)
-
+    return res.status(200).json(expense);
   } catch (error) {
-    return res.status(500).json({error: error.message})
+    return res.status(500).json({ error: error.message });
   }
 }
 
 export async function getTotalByCategory(req, res) {
-  const user = validateAuthorization(req.headers.authorization)
+  const user = validateAuthorization(req.headers.authorization);
 
-  if(!user) {
-    return res.status(401).json({message: "Unauthorized"})
+  if (!user) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
   try {
+    const payload = [];
+    const totalCategories = await Category.findAll();
 
-    const payload = []
-    const totalCategories = await Category.findAll()
-
-    for(const category of totalCategories){
-      const expenseByCategory = await Expense.findAll({ where: {category: category.id } });
+    for (const category of totalCategories) {
+      const expenseByCategory = await Expense.findAll({
+        where: { category: category.id },
+      });
       const totalExpense = expenseByCategory.reduce((accumulator, expense) => {
         return accumulator + expense.value;
-      }, 0)
-      payload.push({category: category.name, quantity: expenseByCategory.length, total: totalExpense})
-
+      }, 0);
+      payload.push({
+        category: category.name,
+        quantity: expenseByCategory.length,
+        total: totalExpense,
+      });
     }
 
-    return res.status(200).json(payload)
-
+    return res.status(200).json(payload);
   } catch (error) {
-      return res.status(500).json({error: error.message})
-    }
+    return res.status(500).json({ error: error.message });
+  }
 }
 
 export async function getTotalByUser(req, res) {
-  const user = validateAuthorization(req.headers.authorization)
+  const user = validateAuthorization(req.headers.authorization);
 
-  if(!user) {
-    return res.status(401).json({message: "Unauthorized"})
+  if (!user) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
   try {
+    const payload = [];
+    const totalUsers = await User.findAll();
 
-    const payload = []
-    const totalUsers = await User.findAll()
-
-    for(const user of totalUsers){
-      const expenseByCategory = await Expense.findAll({ where: {category: user.id } });
-      const totalExpense = expenseByCategory.reduce((accumulator, expense) => {
+    for (const user of totalUsers) {
+      const expenseByUser = await Expense.findAll({
+        where: { userId: user.id },
+      });
+      const totalExpense = expenseByUser.reduce((accumulator, expense) => {
         return accumulator + expense.value;
-      }, 0)
-      payload.push({name: `${user.firstName} ${user.lastName}`, total: totalExpense})
-
+      }, 0);
+      payload.push({
+        name: `${user.firstName} ${user.lastName}`,
+        quantity: expenseByUser.length,
+        total: totalExpense,
+      });
     }
 
-    return res.status(200).json(payload)
-
+    return res.status(200).json(payload);
   } catch (error) {
-      return res.status(500).json({error: error.message})
-    }
+    return res.status(500).json({ error: error.message });
+  }
 }
 
 export async function getTotalByPaymentDate(req, res) {
-  const user = validateAuthorization(req.headers.authorization)
+  const user = validateAuthorization(req.headers.authorization);
 
-  if(!user) {
-    return res.status(401).json({message: "Unauthorized"})
+  if (!user) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
   try {
-    const payload = []
+    const payload = [];
     const expensesByPaymentDate = await Expense.findAll({
       where: {
         paymentDate: {
-          [Op.ne]: null
-        }
-      }
-    })
+          [Op.ne]: null,
+        },
+      },
+    });
 
-    const expensesByMonth = expensesByPaymentDate.map(expense => {
-      const paymentMonth = getMonthName(expense.paymentDate)
-      return {month: paymentMonth, value: expense.value}
-    })
+    const expensesByMonth = expensesByPaymentDate.map((expense) => {
+      const paymentMonth = getMonthName(expense.paymentDate);
+      return { month: paymentMonth, value: expense.value };
+    });
 
     const summedData = expensesByMonth.reduce((acc, item) => {
       if (acc[item.month]) {
-          acc[item.month].total += item.value;
+        acc[item.month].total += item.value;
       } else {
-          acc[item.month] = { month: item.month, total: item.value };
+        acc[item.month] = {
+          month: item.month,
+          quantity: expensesByMonth.length,
+          total: item.value,
+        };
       }
       return acc;
     }, {});
-  
+
     const result = Object.values(summedData);
 
-    return res.status(200).json(result)
-
+    return res.status(200).json(result);
   } catch (error) {
-      return res.status(500).json({error: error.message})
-    }
+    return res.status(500).json({ error: error.message });
+  }
 }
