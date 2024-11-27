fbac8b5fbba986c5db7bb74fe42bda5a1b1e72ca
ryanoliveir <ryandehallo229@gmail.com>
2024-11-26 23:29:53 -0300

    add: database rds

--- config/database.js
+++ config/database.js
@@ -1,8 +1,19 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './db.sqlite', 
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, // Ensure SSL is required
+      rejectUnauthorized: false, // Accept self-signed certificates (Neon often requires this)
+    },
+  },
+  logging: false, // Disable SQL logging in production
+  schema: 'budget-control'
 });
 
 export default sequelize;
